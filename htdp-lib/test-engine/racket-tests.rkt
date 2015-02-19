522d71988e79427f76166fa49f5295f8e247d583
Matthias Felleisen <matthias@ccs.neu.edu>
2015-02-19 11:43:58 -0500

    get check-satisfied to work properly in *SL, including error messages

--- htdp-lib/test-engine/racket-tests.rkt
+++ htdp-lib/test-engine/racket-tests.rkt
@@ -5,11 +5,11 @@
 ;; *****************************************
 
 (require lang/private/teachprims
-         (for-syntax racket/base
-                     lang/private/rewrite-error-message)
+         (for-syntax racket/base lang/private/rewrite-error-message)
          racket/class
          racket/match
-         racket/function
+         ; racket/function
+         htdp/error
          lang/private/continuation-mark-key
          lang/private/rewrite-error-message
          (for-syntax #;"requiring from" lang/private/firstorder #;"avoids load cycle")
@@ -189,30 +189,54 @@
   (syntax-case stx ()
     [(_ actual:exp expected-property:id)
      (identifier? #'expected-property:id)
-     (let* ([name (symbol->string (syntax-e #'expected-property:id))]
-            [prop (first-order->higher-order #'expected-property:id)])
+     (let* ([prop (first-order->higher-order #'expected-property:id)]
+            [name (symbol->string (syntax-e  #'expected-property:id))]
+            [code 
+             #`(lambda (x)
+                 (with-handlers ([exn:fail:contract:arity?
+                                  (lambda (x) 
+                                    (error-check (lambda (v) #f) #,name SATISFIED-FMT #t))])
+                   (#,prop x)))])
        (check-expect-maker stx 
                            #'check-values-property 
                            #'actual:exp
-                           (list prop name)
+                           (list code name)
+                           'comes-from-check-satisfied))]
+    [(_ actual:exp expected-property:exp)
+     (let* ([prop #'(let ([p? expected-property:exp])
+                       (unless (and (procedure? p?) (procedure-arity-includes? p? 1))
+                         (define name (object-name p?))
+                         (if name
+                             (error-check (lambda (v) #f) name SATISFIED-FMT #t)
+                             (error-check (lambda (v) #f) p? SATISFIED-FMT #t)))
+                      p?)])
+       (check-expect-maker stx 
+                           #'check-values-property 
+                           #'actual:exp
+                           (list prop "unknown name")
                            'comes-from-check-satisfied))]
     [(_ actual:exp expected-property:exp) 
      (raise-syntax-error 'check-satisfied "expects named function in second position." stx)]
     [_ (raise-syntax-error 'check-satisfied (argcount-error-message/stx 2 stx) stx)]))
 
-(define (check-values-property test actual property? src test-engine)
+(define (check-values-property test actual name src test-engine)
   ;; it is okay if actual is a procedure because property testing may use
   ;; it, but it is possibly weird for students
-  (send (send test-engine get-info) add-check)
   (run-and-check
    ;; check
    (lambda (p? v _what-is-this?)
-     (unless (and (procedure? p?)
-                  (procedure-arity-includes? p? 1))
-       (error-check (lambda (v) #f) property? SATISFIED-FMT #t))
-     (p? v))
+     (unless (and (procedure? p?) (procedure-arity-includes? p? 1))
+       (error-check (lambda (v) #f) name SATISFIED-FMT #t))
+     (define r (p? v))
+     (cond
+       [(boolean? r) 
+        (send (send test-engine get-info) add-check)
+        r]
+       [else 
+        (error-check (lambda (v) #f) name "ouch ouch" #t)
+        (check-result (format "~a [as predicate in check-satisfied]" name) boolean? "boolean" r)]))
    ;; maker
-   (lambda (src format v1 _v2 _) (make-satisfied-failed src format v1 property?))
+   (lambda (src format v1 _v2 _) (make-satisfied-failed src format v1 name))
    ;; test 
    test
    ;; expect 
@@ -220,7 +244,7 @@
    #f
    src
    test-engine
-   (list 'check-satisfied property?)))
+   (list 'check-satisfied name)))
 
 ;; check-values-expected: (-> scheme-val) (-> nat scheme-val) src test-engine -> void
 (define (check-random-values test actual-maker src test-engine)
@@ -553,3 +577,8 @@
 
 (provide scheme-test-data test-format test-execute test-silence error-handler 
          signature-test-info% build-test-engine)
+
+; (check-satisfied 1 equal?)
+; (check-satisfied 1 (values odd?))
+; (check-satisfied (random 10) 11)
+; (test)
