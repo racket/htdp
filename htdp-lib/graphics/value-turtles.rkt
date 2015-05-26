1e9c1477f77fb3c3e1ae813e53be0dc55d877b24
Robby Findler <robby@racket-lang.org>
2015-05-25 21:25:04 -0500

    add contracts to the value turtles

--- htdp-lib/graphics/value-turtles.rkt
+++ htdp-lib/graphics/value-turtles.rkt
@@ -1,3 +1,20 @@
 #lang racket/base
-(require "private/value-turtles.rkt")
-(provide turtles move draw turn turn/radians merge clean turtles?)
+(require "private/value-turtles.rkt"
+         racket/contract)
+(provide
+ (contract-out
+  [turtles? (-> any/c boolean?)]
+  [turtles (case->
+            (-> (and/c real? positive?)
+                (and/c real? positive?)
+                turtles?)
+            (-> (and/c real? positive?)
+                (and/c real? positive?)
+                real? real? real?
+                turtles?))]
+  [move (-> real? turtles? turtles?)]
+  [draw (-> real? turtles? turtles?)]
+  [turn (-> real? turtles? turtles?)]
+  [turn/radians (-> real? turtles? turtles?)]
+  [merge (->* (turtles?) #:rest (listof turtles?) turtles?)]
+  [clean (-> turtles? turtles?)]))
