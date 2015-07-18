1f009e21042ec2c41db80cbe47c3f973402420c5
Robby Findler <robby@racket-lang.org>
2015-07-18 10:38:16 -0500

    skip some tests that open up GUIs
    (thanks to the recent #lang htdp change that
    puts a test submodule in via #%module-begin)

--- htdp-test/htdp/info.rkt
+++ htdp-test/htdp/info.rkt
@@ -2,4 +2,11 @@
 
 (define name "HtDP Teachpack Tests")
 (define compile-omit-paths '("tests"))
-
+(define test-omit-paths '("tests/arrow-gui.rkt"
+                          "tests/arrow.rkt"
+                          "tests/draw.rkt"
+                          "tests/guess1.rkt"
+                          "tests/guess2.rkt"
+                          "tests/guess3.rkt"
+                          "tests/lkup-gui.rkt"
+                          "tests/master.rkt"))
