55b9623269770c4a3c9ec268a13c548e42832038
Sam Tobin-Hochstadt <samth@cs.indiana.edu>
2015-08-14 15:46:47 -0400

    Don't run these files, since they are not expected to work.

--- htdp-test/tests/test-engine/info.rkt
+++ htdp-test/tests/test-engine/info.rkt
@@ -1,3 +1,7 @@
 #lang info
 
 (define test-responsibles '((all kathyg)))
+
+(define test-omit-paths '("check-satisfied2.rkt"
+                          "check-satisfied4.rkt"
+                          "check-satisfied5.rkt"))
