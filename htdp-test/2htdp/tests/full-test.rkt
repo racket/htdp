53f2549413f3b60ea696fd2b19798de2d503ef3a
Matthias Felleisen <matthias@ccs.neu.edu>
2015-10-05 17:36:55 -0400

    tests for display-mode in big-bang

---
+++ htdp-test/2htdp/tests/full-test.rkt
@@ -0,0 +1,12 @@
+#lang htdp/isl+
+
+(require 2htdp/universe)
+(require 2htdp/image)
+
+;; {'fullscreen, 'normal} -> Number 
+(define (main x)
+  (big-bang 900
+    (to-draw (lambda (x) (circle (+ 20 x) 'solid 'red)))
+    (on-tick (lambda (x) (- x 20)))
+    (stop-when (lambda (x) (< x 0)))
+    (display-mode x)))
