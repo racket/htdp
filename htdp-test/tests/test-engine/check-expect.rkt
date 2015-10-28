592110a7165220dc3a22036dad6ba7c85687678f
Robby Findler <robby@racket-lang.org>
2015-10-28 10:19:56 -0500

    add test case for 22c94cd0f2

---
+++ htdp-test/tests/test-engine/check-expect.rkt
@@ -0,0 +1,26 @@
+#lang racket/base
+(require (for-syntax racket/base)
+         rackunit)
+
+(define-syntax (run stx)
+  (syntax-case stx ()
+    [(build name exp ...)
+     (let ([modname (string->symbol (format "mod~a" (syntax-line stx)))])
+       (datum->syntax
+        stx
+        `(begin
+           (module ,modname racket/base
+             (require test-engine/racket-tests)
+             (provide get-output)
+             ,@(syntax->list #'(exp ...))
+             (define sp (open-output-string))
+             (parameterize ([current-output-port sp])
+               (test))
+             (define (get-output) (get-output-string sp)))
+           (require (submod "." ,modname))
+           (define ,#'name (get-output)))))]))
+
+(run ch1 (check-expect "hello" "world"))
+(check-regexp-match (regexp-quote "Actual value: \"hello\"") ch1)
+(check-regexp-match (regexp-quote "Expected value: \"world\"") ch1)
+
