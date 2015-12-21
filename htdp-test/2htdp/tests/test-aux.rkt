86f546c392c4f8e2f5686b21dc046fffda810601
Matthew Flatt <mflatt@racket-lang.org>
2015-12-21 15:07:10 -0700

    fix `2htdp/universe` tests
    The test harness performed some work in the wrong thread,
    creating races that sometimes caused timeout or bad
    results.

--- htdp-test/2htdp/tests/test-aux.rkt
+++ htdp-test/2htdp/tests/test-aux.rkt
@@ -14,4 +14,18 @@
 (define-syntax-rule
   (testing e ...)
   (begin ;; module+ test
-    (parameterize ((current-eventspace (make-eventspace))) e ...)))
+    (do-testing (lambda () e ...))))
+
+(define (do-testing thunk)
+  (parameterize ((current-eventspace (make-eventspace)))
+    (define ch (make-channel))
+    (queue-callback
+     (lambda ()
+       (with-handlers ([exn? (lambda (e) (channel-put ch e))])
+         (channel-put
+          ch
+          (call-with-values thunk list)))))
+    (define r (channel-get ch))
+    (if (list? r)
+        (apply values r)
+        (raise r))))
