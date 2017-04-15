6ef98833082fd12f21f83e77a46a73a332b614d8
Matthias Felleisen <matthias@ccs.neu.edu>
2017-04-15 10:10:12 -0400

    omit ..9

--- htdp-test/tests/test-engine/info.rkt
+++ htdp-test/tests/test-engine/info.rkt
@@ -5,4 +5,5 @@
 (define test-omit-paths '("check-satisfied2.rkt"
                           "check-satisfied4.rkt"
                           "check-satisfied5.rkt"
-                          "check-satisfied8.rkt"))
+                          "check-satisfied8.rkt"
+			  "check-satisfied9.rkt"))
