59b1c67ec921de3f9661f4cbc04a5e58cb2082b6
Matthew Flatt <mflatt@racket-lang.org>
2018-06-13 08:58:16 -0600

    use `include-bitmap` for `big-bang` bitmaps
    Using `include-bitmap` instead of `define-runtime-path` should make
    big-bang programs work as truly stand-alone ".exe"s on Windows ---
    since 'embed-dlls? is already enabled.

--- htdp-lib/2htdp/private/pad.rkt
+++ htdp-lib/2htdp/private/pad.rkt
@@ -2,7 +2,7 @@
 
 ;; provide basic elements for game pad clause in big-bang: the icon, pad-event?
 
-(require racket/runtime-path)
+(require mrlib/include-bitmap)
 
 (provide
   ;; bitmap
@@ -17,8 +17,7 @@
 
 ;; ---------------------------------------------------------------------------------------------------
 
-(define-runtime-path gamepad-path "gamepad.png")
-(define game-pad (read-bitmap gamepad-path 'png/alpha #f #t))
+(define game-pad (include-bitmap "gamepad.png" 'png/alpha))
 (unless (send game-pad ok?)
   (error 'big-bang "the game pad icon isn't available; please report error"))
 
