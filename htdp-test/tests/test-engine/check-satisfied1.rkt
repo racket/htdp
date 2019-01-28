793e84a8dec71fb88240212b7d6e1348abeb0677
Matthias Felleisen <matthias@ccs.neu.edu>
2019-01-28 10:28:40 -0500

    format of saved file changed

--- htdp-test/tests/test-engine/check-satisfied1.rkt
+++ htdp-test/tests/test-engine/check-satisfied1.rkt
@@ -1,6 +1,6 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname check-satisfied1) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname check-satisfied1) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 ;; ASSUME: THIS FILE is IN BSL 
 
 (define (id x) x)
