71e3319464fc5063b2ef2516b6cc7b044959b59f
Matthias Felleisen <matthias@ccs.neu.edu>
2019-11-13 21:18:33 -0500

    annotated with comments

--- htdp-test/tests/test-engine/check-ordered.rkt
+++ htdp-test/tests/test-engine/check-ordered.rkt
@@ -1,4 +1,11 @@
-#lang htdp/bsl
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname check-ordered) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; #lang htdp/bsl
+
+;; this test should not be reported as failing:
+;; 1. it passes
+;; 2. it is f that is called before defined not g 
 
 (check-expect (g 30) "goodbye")
 (f 40)
