b0204786f2fd6e37a4ee9dd6c7fd5087a5748fff
Matthias Felleisen <matthias@ccs.neu.edu>
2020-07-29 13:35:21 -0400

    mark as

--- htdp-test/tests/test-engine/check-ordered.rkt
+++ htdp-test/tests/test-engine/check-ordered.rkt
@@ -7,7 +7,8 @@
 ;; 1. it passes
 ;; 2. it is f that is called before defined not g 
 
-;; OUCH :: I can't figure out how to run DrRacket's older than 7. but I am pretty sure I had gotten this right. 
+;; OUCH :: expected to fail, get your work done
+;; This is issue #87, which you never chased down 
 
 (check-expect (g 30) "goodbye")
 (f 40)
