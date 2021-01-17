abbb95a2e8ef7d785e8a049e8ee984fca48b1dcb
John Clements <clements@racket-lang.org>
2021-01-17 11:34:39 -0800

    enable stepper button in #lang teaching languages
    Create persistent compile-time name for stepper button
    Code by @mikesperber

--- htdp-lib/stepper/drracket-button.rkt
+++ htdp-lib/stepper/drracket-button.rkt
@@ -1,8 +1,17 @@
 #lang scheme/base
 (require scheme/class string-constants/string-constant
-         (prefix-in x: "private/step-img.rkt"))
-(provide stepper-button-callback stepper-drracket-button)
-(define-local-member-name stepper-button-callback)
+         (prefix-in x: "private/step-img.rkt")
+         (for-syntax racket/base))
+(provide stepper-drracket-button stepper-button-callback)
+
+; hack to make sure the key gets generated once at compile time, and
+; not each time this module is instantiated
+(define-syntax (unique-member-name-key stx)
+  (syntax-case stx ()
+    ((_)
+     #`(member-name-key #,(gensym 'stepper-button-callback)))))
+
+(define-member-name stepper-button-callback (unique-member-name-key))
 
 (define stepper-drracket-button
   (list 
