bf73126668b0b07b57df6cbd8d8f946fa576c5d8
John Clements <clements@racket-lang.org>
2021-04-26 09:00:05 -0700

    isl+ is the one that should show lambdas
    test case still required

--- htdp-lib/htdp/isl+/lang/reader.rkt
+++ htdp-lib/htdp/isl+/lang/reader.rkt
@@ -1,4 +1,5 @@
 #lang s-exp htdp/bsl/reader
 lang/htdp-intermediate-lambda
 '(abbreviate-cons-as-list
+  use-function-output-syntax
   read-accept-quasiquote)
--- htdp-lib/htdp/isl/lang/reader.rkt
+++ htdp-lib/htdp/isl/lang/reader.rkt
@@ -1,5 +1,4 @@
 #lang s-exp htdp/bsl/reader
 lang/htdp-intermediate
 '(abbreviate-cons-as-list
-  use-function-output-syntax
   read-accept-quasiquote)
