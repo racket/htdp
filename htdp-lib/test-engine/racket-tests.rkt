racket/htdp

commit ae916f0b222df93b2f081ff9510b146957083543

test-engine: generate markup for exception via error-display-handler
This way, for #lang teaching languages, we can leverage DrRacket's
error-display-handler to extract srcloc information from the
exception.

htdp-lib/test-engine/racket-tests.rkt
@@ -20,7 +20,10 @@
                      #;"requiring from" lang/private/firstorder #;"avoids load cycle")
          test-engine/test-engine
          (only-in test-engine/test-markup get-rewritten-error-message)
-         test-engine/syntax)
+         test-engine/syntax
+         racket/class
+         simple-tree-text-markup/construct
+         simple-tree-text-markup/port)
 
 (define INEXACT-NUMBERS-FMT
   "check-expect cannot compare inexact numbers. Try (check-within test ~a range).")
@@ -70,9 +73,25 @@
             (if (and (not (= found 0)) fn-is-large) "only " "")
             (if (= found 0) "none" found))))
 
+(define (exn->markup exn)
+  (let-values (([port get-markup] (make-markup-output-port/unsafe (lambda (special)
+                                                                    (cond
+                                                                      [(and (object? special)
+                                                                            (is-a? special srclocs-special<%>)
+                                                                            (send special get-srclocs))
+                                                                       => (lambda (srclocs)
+                                                                            (if (and (pair? srclocs)
+                                                                                     (srcloc? (car srclocs)))
+                                                                                (srcloc-markup (car srclocs) (srcloc->string (car srclocs)))
+                                                                                empty-markup))]
+                                                                      [else empty-markup])))))
+    (parameterize ([current-error-port port])
+      ((error-display-handler) (exn-message exn) exn)
+      (get-markup))))
+
 (define (make-exn->unexpected-error src expected)
   (lambda (exn)
-    (unexpected-error src expected exn)))
+    (unexpected-error/markup src expected exn (exn->markup exn))))
 
 (define-syntax (check-expect stx)
   (check-context! 'check-expect CHECK-EXPECT-DEFN-STR stx)
@@ -179,7 +198,7 @@
           (satisfied-failed src actual name)]
          [else #t])))
    (lambda (exn)
-     (unsatisfied-error src name exn))))
+     (unsatisfied-error/markup src name exn (exn->markup exn)))))
 
 (define-syntax (check-within stx)
   (check-context! 'check-within CHECK-WITHIN-DEFN-STR stx)
@@ -223,7 +242,7 @@
                         (let ((msg (get-rewritten-error-message exn)))
                           (if (equal? msg error)
                               #t
-                              (incorrect-error src error exn))))])
+                              (incorrect-error/markup src error exn (exn->markup exn)))))])
        (let ([actual (test)])
          (expected-error src error actual))))
    (make-exn->unexpected-error src error))) ; probably can't happen

htdp-doc/test-engine/test-engine.scrbl
@@ -216,6 +216,15 @@ The @code{srcloc} is the source location of the check.}
 An error happened instead of regular termination.
 }
 
+@defstruct*[(unexpected-error/markup unexpected-error)
+            ((srcloc srcloc?)
+             (expected any/c)
+             (exn exn?)
+	     (error-markup markup?))]{
+An error happened instead of regular termination.  This also contains markup
+describing the error.
+}
+
 @defstruct*[(unequal fail-reason)
             ((srcloc srcloc?)
              (actual any/c)
@@ -238,6 +247,16 @@ Generated by @racket[check-within].
              (expected any/c)
              (exn exn?))]{
 An exception was expected, but a different one occurred.
+Generated by @racket[check-error].
+}
+
+@defstruct*[(incorrect-error/markup incorrect-error)
+            ((srcloc srcloc?)
+             (expected any/c)
+             (exn exn?)
+	     (error-markup markup?))]{
+An exception was expected, but a different one occurred.
+Also includes markup describing the error.
 Generated by @racket[check-error].
 }
 
@@ -281,6 +300,17 @@ Generated by @racket[check-satisfied].
              (exn exn?))]{
 A value was supposed to satsify a predicate, but an error happened
 instead.  The @code{name} field is the name of the predicate.
+Generated by @racket[check-satisfied].
+}
+
+@defstruct*[(unsatisfied-error/markup unsatisfied-error)
+            ((srcloc srcloc?)
+             (name string?)
+             (exn exn?)
+	     (error-markup markup?))]{
+A value was supposed to satsify a predicate, but an error happened
+instead.  The @code{name} field is the name of the predicate.
+Also includes markup describing the error.
 Generated by @racket[check-satisfied].
 }
 

htdp-lib/test-engine/test-engine.rkt
@@ -1,6 +1,7 @@
 ; Manage the running and recording of tests and their failures
 #lang racket/base
-(require racket/contract)
+(require racket/contract
+         (only-in simple-tree-text-markup/data markup?))
 
 (provide (contract-out
           (struct test-object
@@ -24,17 +25,29 @@
           ; srcloc the source location of the actual check
           (struct fail-reason ((srcloc srcloc?)))
 
-          ; an error happened instead of an expected valuee
+          ; an error happened instead of an expected value
           (struct (unexpected-error fail-reason)
             ((srcloc srcloc?)
              (expected any/c)
              (exn exn?)))
+
+          (struct (unexpected-error/markup unexpected-error)
+            ((srcloc srcloc?)
+             (expected any/c)
+             (exn exn?)
+             (error-markup markup?)))
 
           ; wanted to satisfy a predicate, but error happend
           (struct (unsatisfied-error fail-reason)
             ((srcloc srcloc?)
              (name string?)
              (exn exn?)))
+
+          (struct (unsatisfied-error/markup unsatisfied-error)
+            ((srcloc srcloc?)
+             (name string?)
+             (exn exn?)
+             (error-markup markup?)))
 
           ; some result came out, but not the one we expected
           (struct (unequal fail-reason)
@@ -54,6 +67,12 @@
             ((srcloc srcloc?)
              (expected string?)
              (exn exn?)))
+
+          (struct (incorrect-error/markup incorrect-error)
+            ((srcloc srcloc?)
+             (expected string?)
+             (exn exn?)
+             (error-markup markup?)))
 
           ; we expected an error, but a value came out instead
           (struct (expected-error fail-reason)
@@ -181,7 +200,13 @@
 (struct unexpected-error fail-reason (expected exn)
   #:transparent)
 
+(struct unexpected-error/markup unexpected-error (error-markup)
+  #:transparent)
+
 (struct unsatisfied-error fail-reason (name exn)
+  #:transparent)
+
+(struct unsatisfied-error/markup unsatisfied-error (error-markup)
   #:transparent)
 
 (struct unequal fail-reason (actual expected)
@@ -191,6 +216,9 @@
   #:transparent)
 
 (struct incorrect-error fail-reason (expected exn)
+  #:transparent)
+
+(struct incorrect-error/markup incorrect-error (error-markup)
   #:transparent)
 
 (struct expected-error fail-reason (message value)

htdp-lib/test-engine/test-markup.rkt
@@ -12,7 +12,10 @@
          (only-in racket/list append-map)
          string-constants
          test-engine/test-engine
-         (only-in simple-tree-text-markup/data markup?)
+         (only-in simple-tree-text-markup/data
+                  markup?
+                  srcloc-markup? srcloc-markup-srcloc
+                  transform-markup)
          simple-tree-text-markup/construct
          simple-tree-text-markup/text
          simple-tree-text-markup/port
@@ -161,6 +164,12 @@
         (format (string-constant test-engine-at-line-column)
                 line col))))
 
+; provide properly translated text for srclocs
+(define (markup-reformat-srcloc markup)
+  (transform-markup `((,srcloc-markup? . ,(lambda (srcloc _markup)
+                                            (srcloc-markup srcloc (format-srcloc srcloc)))))
+                    markup))
+
 (define (format->markup format-string . vals)
   (let loop ((chars (string->list format-string))
              (vals vals)
@@ -182,6 +191,14 @@
                 (cdr vals)
                 (cons (framed-markup (value->markup (car vals))) rev-markups)
                 rev-lines))
+         ((#\a #\A)
+          (let ((val (car vals)))
+            (loop (cddr chars)
+                  (cdr vals)
+                  (if (markup? val)
+                      (cons (markup-reformat-srcloc val) rev-markups)
+                      (cons (format "~a" (car vals)) rev-markups))
+                  rev-lines)))
          (else
           (loop (cddr chars)
                 (cdr vals)
@@ -206,15 +223,27 @@
 
 (define (reason->markup fail)
   (cond
+    [(unexpected-error/markup? fail)
+     (format->markup (string-constant test-engine-check-encountered-error)
+                     (unexpected-error-expected fail)
+                     (unexpected-error/markup-error-markup fail))]
+     
     [(unexpected-error? fail)
      (format->markup (string-constant test-engine-check-encountered-error)
                      (unexpected-error-expected fail)
                      (get-rewritten-error-message (unexpected-error-exn fail)))]
+
+    [(unsatisfied-error/markup? fail)
+     (format->markup
+      "check-satisfied for ~a encountered an error.\n  :: ~a"
+      (unsatisfied-error-name fail)
+      (unsatisfied-error/markup-error-markup fail))]
     [(unsatisfied-error? fail)
      (format->markup
       "check-satisfied for ~a encountered an error.\n  :: ~a"
       (unsatisfied-error-name fail)
       (get-rewritten-error-message (unsatisfied-error-exn fail)))]
+
     [(unequal? fail)
      (format->markup (string-constant test-engine-actual-value-differs-error)
                      (unequal-actual fail)
@@ -233,10 +262,16 @@
                          (not-within-actual fail)
                          (not-within-expected fail)
                          (not-within-range fail)))]
+    
+    [(incorrect-error/markup? fail)
+     (format->markup (string-constant test-engine-encountered-error-error)
+                     (incorrect-error-expected fail)
+                     (incorrect-error/markup-error-markup fail))]
     [(incorrect-error? fail)
      (format->markup (string-constant test-engine-encountered-error-error)
                      (incorrect-error-expected fail)
                      (get-rewritten-error-message (incorrect-error-exn fail)))]
+    
     [(expected-error? fail)
      (cond
        ((expected-error-message fail)
@@ -379,9 +414,19 @@
     (failed-check
      (unexpected-error (srcloc 'source 1 0 10 20) 'expected (exn "not expected" (current-continuation-marks)))
      (srcloc 'exn 2 1 30 40)))
+  (define fail-unexpected-error/markup
+    (failed-check
+     (unexpected-error/markup (srcloc 'source 1 0 10 20) 'expected (exn "not expected" (current-continuation-marks))
+                              (vertical "line1" "line2"))
+     (srcloc 'exn 2 1 30 40)))
   (define fail-unsatisfied-error
     (failed-check
      (unsatisfied-error (srcloc 'source 1 0 10 20) "zero?" (exn "not expected" (current-continuation-marks)))
+     #f))
+  (define fail-unsatisfied-error/markup
+    (failed-check
+     (unsatisfied-error/markup (srcloc 'source 1 0 10 20) "zero?" (exn "not expected" (current-continuation-marks))
+                               (vertical "line1" "line2"))
      #f))
   (define fail-unequal
     (failed-check
@@ -394,6 +439,11 @@
   (define fail-incorrect-error
     (failed-check
      (incorrect-error (srcloc 'source 1 0 10 20) "expected" (exn "not expected" (current-continuation-marks)))
+     #f))
+  (define fail-incorrect-error/markup
+    (failed-check
+     (incorrect-error/markup (srcloc 'source 1 0 10 20) "expected" (exn "not expected" (current-continuation-marks))
+                             (vertical "line1" "line2"))
      #f))
   (define fail-expected-error
     (failed-check
@@ -465,8 +515,10 @@
    markup?
    (test-object->markup
     (make-test-object (list void void)
-                      (list fail-unexpected-error fail-unsatisfied-error
-                            fail-unequal fail-not-within fail-incorrect-error
+                      (list fail-unexpected-error fail-unexpected-error/markup
+                            fail-unsatisfied-error fail-unsatisfied-error/markup
+                            fail-unequal fail-not-within
+                            fail-incorrect-error fail-incorrect-error/markup
                             fail-not-mem fail-not-range fail-satisfied-failed
                             fail-property-fail fail-property-error
                             fail-violated-signature)

commit 21d52f728e9af2b3a2aacadcddc59788460e45a2

Unbreak automatic-tests.
Breakage came from ae916f0b222df93b2f081ff9510b146957083543,
as we started using error-display-handler to render markup
for exceptions during tests.  The stepper uses these to detect
errors.

htdp-lib/stepper/private/model.rkt
@@ -52,6 +52,7 @@
          "macro-unwind.rkt"
          "lifting.rkt"
          (only-in test-engine/syntax test-silence)
+         (only-in test-engine/racket-tests test-engine-is-using-error-display-handler?)
          test-engine/test-markup
          
          ;; for breakpoint display
@@ -509,18 +510,19 @@
   ;; the error (along with the held "before" step if present
   (define (err-display-handler _message exn)
     (define message (get-rewritten-error-message exn))
-    (match maybe-held-exp
-      [(struct skipped-step ())
-       (receive-result (Error-Result message))]
-      [(struct no-sexp ())
-        (receive-result (Error-Result message))]
-      [(struct held (exps dc posn-info))
-       (begin
-         (receive-result
-          (Before-Error-Result (map sstx (append held-finished-list exps))
-                               message
-                               posn-info))
-         (set-maybe-held-exp! the-no-sexp))]))
+    (unless (test-engine-is-using-error-display-handler?)
+      (match maybe-held-exp
+        [(struct skipped-step ())
+         (receive-result (Error-Result message))]
+        [(struct no-sexp ())
+         (receive-result (Error-Result message))]
+        [(struct held (exps dc posn-info))
+         (begin
+           (receive-result
+            (Before-Error-Result (map sstx (append held-finished-list exps))
+                                 message
+                                 posn-info))
+           (set-maybe-held-exp! the-no-sexp))])))
 
   (program-expander
    (lambda () ; init

htdp-lib/test-engine/racket-tests.rkt
@@ -10,7 +10,8 @@
          check-satisfied ;; syntax : (check-satisfied <expression> <expression>)
          ;; re-exports from test-engine/syntax
          test-execute test-silence
-         test) 
+         test
+         test-engine-is-using-error-display-handler?) 
 
 (require lang/private/teachprims
          racket/match
@@ -73,6 +74,11 @@
             (if (and (not (= found 0)) fn-is-large) "only " "")
             (if (= found 0) "none" found))))
 
+; The stepper needs this: it binds error-display-handler to get notified of an error.
+; However, we call (error-display-handler) just to generate markup, and the stepper
+; needs to ignore that.
+(define test-engine-is-using-error-display-handler? (make-parameter #f))
+
 (define (exn->markup exn)
   (let-values (([port get-markup] (make-markup-output-port/unsafe (lambda (special)
                                                                     (cond
@@ -85,9 +91,10 @@
                                                                                 (srcloc-markup (car srclocs) (srcloc->string (car srclocs)))
                                                                                 empty-markup))]
                                                                       [else empty-markup])))))
-    (parameterize ([current-error-port port])
-      ((error-display-handler) (exn-message exn) exn)
-      (get-markup))))
+    (parameterize ([current-error-port port]
+                   [test-engine-is-using-error-display-handler? #t])
+        ((error-display-handler) (exn-message exn) exn)
+        (get-markup))))
 
 (define (make-exn->unexpected-error src expected)
   (lambda (exn)