racket/htdp

commit 8a6d34936606134e73ccf5f4b687eb7d8068cab0

Print functions as "function:f" instead of "function"
... in BSL, BSL+, ISL.

htdp-lib/lang/private/signature-syntax.rkt
@@ -185,7 +185,10 @@
     (syntax-case stx ()
       ((_ ?name ?cnt ?expr)
        (with-syntax ((?enforced
-		       (stepper-syntax-property #'(attach-name '?name (apply-signature/blame ?cnt ?expr))
+                      (stepper-syntax-property #'(attach-name '?name
+                                                              (apply-signature/blame ?cnt
+                                                                                     ;; for reporting in signature violations
+                                                                                     (attach-name '?name ?expr)))
 						'stepper-skipto/discard
 						;; apply-signature/blame takes care of itself
 						;; remember there's an implicit #%app

htdp-lib/htdp/bsl/runtime.rkt
@@ -67,7 +67,12 @@
      (lambda (val basic sub)
        (cond
          [(and (sl-runtime-settings-output-function-instead-of-lambda? settings)
-               (procedure? val)) 'function]
+               (procedure? val))
+          (cond
+            ((object-name val)
+             => (lambda (name)
+                  (string->symbol (format "function:~a" name))))
+            (else 'function))]
          [(and (not (sl-runtime-settings-true/false/empty-as-ids? settings)) (equal? val '())) ''()]
          [(equal? val set!-result) '(void)]
          [(signature? val)

commit 38ee27631bcd004e22714e59124025c17fbcf0ef

Unbreak srclocs, stepper for signature declarations.
Fix breakage caused by 8a6d34936606134e73ccf5f4b687eb7d8068cab0:

- have the stepper descend to the right place
- attach explicit, correct srcloc for signature-violation reporting

htdp-lib/lang/private/signature-syntax.rkt
@@ -10,6 +10,7 @@
 	 scheme/promise
 	 (for-syntax scheme/base)
 	 (for-syntax syntax/stx)
+         (for-syntax racket/syntax)
          (for-syntax stepper/private/syntax-property)
 	 (for-syntax "firstorder.rkt"))
 
@@ -184,16 +185,16 @@
   (lambda (stx)
     (syntax-case stx ()
       ((_ ?name ?cnt ?expr)
-       (with-syntax ((?enforced
-                      (stepper-syntax-property #'(attach-name '?name
-                                                              (apply-signature/blame ?cnt
-                                                                                     ;; for reporting in signature violations
-                                                                                     (attach-name '?name ?expr)))
-						'stepper-skipto/discard
-						;; apply-signature/blame takes care of itself
-						;; remember there's an implicit #%app
-						'(syntax-e cdr syntax-e cdr cdr car))))
-							   
+       (with-syntax* (; for reporting in signature violations
+                      (?with-name (stepper-syntax-property (syntax/loc #'?expr (attach-name '?name ?expr))
+                                                           'stepper-skipto/discard
+                                                           '(syntax-e cdr syntax-e cdr cdr car)))
+                      (?enforced (stepper-syntax-property #'(attach-name '?name
+                                                                         (apply-signature/blame ?cnt ?with-name))
+                                                          'stepper-skipto/discard
+                                                          ;; apply-signature/blame takes care of itself
+                                                          ;; remember there's an implicit #%app
+                                                          '(syntax-e cdr syntax-e cdr cdr car))))
 	 #'(define ?name ?enforced))))))
 
 (define-syntax define-values/signature