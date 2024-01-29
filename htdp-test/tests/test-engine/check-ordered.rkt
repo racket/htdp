5f706c2c6f4ea4e808ed51a335937b2bea477dbb
Matthias Felleisen <matthias@ccs.neu.edu>
2024-01-29 10:46:40 -0500

    the issue disappeared

--- htdp-test/tests/test-engine/check-ordered.rkt
+++ htdp-test/tests/test-engine/check-ordered.rkt
@@ -3,12 +3,9 @@
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname check-ordered) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 ; #lang htdp/bsl
 
-;; this test should not be reported as failing:
-;; 1. it passes
-;; 2. it is f that is called before defined not g 
-
-;; OUCH :: expected to fail, get your work done
-;; This is issue #87, which you never chased down 
+;; it is f that is called before defined not g 
+;; SOLVED #87 by Mike S.'s fix of other problem
+;; (but the rendering of the output sucks. It seems to lack a newline.)
 
 (check-expect (g 30) "goodbye")
 (f 40)
