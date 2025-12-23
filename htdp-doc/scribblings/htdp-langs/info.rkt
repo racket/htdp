2b2a070a2ba790beb59d69f74c11938dd154d988
Matthew Flatt <mflatt@racket-lang.org>
2025-12-23 12:01:21 -0700

    declare the HtDP family for listing on a family-choosing page

--- htdp-doc/scribblings/htdp-langs/info.rkt
+++ htdp-doc/scribblings/htdp-langs/info.rkt
@@ -2,3 +2,7 @@
 
 (define scribblings '(("htdp-langs.scrbl" (multi-page) (teaching -12 ("HtDP")))
                       ("htdp-ptr.scrbl" () (teaching -11))))
+
+(define language-family (list (hash 'fam "HtDP"
+                                    'doc '(lib "scribblings/htdp-langs/htdp-langs.scrbl")
+                                    'order -100)))
