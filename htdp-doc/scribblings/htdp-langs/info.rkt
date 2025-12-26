618a13597dff8cb8a69e82508f3c2fbce26d3773
Matthew Flatt <mflatt@racket-lang.org>
2025-12-26 14:36:35 -0700

    info.rkt language-family: 'fam => 'family, 'famroot => 'family-root

--- htdp-doc/scribblings/htdp-langs/info.rkt
+++ htdp-doc/scribblings/htdp-langs/info.rkt
@@ -3,6 +3,6 @@
 (define scribblings '(("htdp-langs.scrbl" (multi-page) (teaching -12 ("HtDP")))
                       ("htdp-ptr.scrbl" () (teaching -11))))
 
-(define language-family (list (hash 'fam "HtDP"
+(define language-family (list (hash 'family "HtDP"
                                     'doc '(lib "scribblings/htdp-langs/htdp-langs.scrbl")
                                     'order -100)))
